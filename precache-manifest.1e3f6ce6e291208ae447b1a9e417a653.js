b280b2d68f9a7b04c84a4a7ce6382e88e4efde75
TheVivekGaur <vvksngh24@gmail.com>
2020-04-21 12:27:18 +0530

    Updates

---
+++ precache-manifest.1e3f6ce6e291208ae447b1a9e417a653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1d2246661e90212c486ad3cd3e8865",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a80f3d47b8d798811198",
+    "url": "/monsters-rolodex/static/css/main.973bbef4.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80f3d47b8d798811198",
+    "url": "/monsters-rolodex/static/js/main.1d2fcb0f.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
